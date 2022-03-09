79b1cf6fa19c2fe93cfc2caf153124d4ef21aa35
EricLi404 <leiflyy@outlook.com>
2022-03-09 14:59:20 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM golang:1.17.8
+
+ENV GO111MODULE=on \
+    GOBIN="/opt/run" \
+    GOPROXY="https://goproxy.cn,https://mirrors.aliyun.com/goproxy,direct" \
+
+WORKDIR /
+
+COPY ./* /opt/build/
+
+WORKDIR /opt/build/
+
+RUN mkdir -p /opt/run/log &&\
+    go env && \
+    go mod vendor -v &&\
+    go install -ldflags "-X 'main.goVersion=$(go version)' -X 'main.gitHash=$(git rev-parse --short HEAD)' -X 'main.buildTime=$(date "+%Y-%m-%d %H:%M:%S")' " &&\
+    echo "build success"
+
+
+CMD ["/opt/run/gosleep"]
--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"os"
 	"time"
 )
 
+var buildTime = ""
+var gitHash = ""
+var goVersion = ""
+
 func main() {
+	args := os.Args
+	if len(args) == 2 && (args[1] == "--version" || args[1] == "-v") {
+		fmt.Printf("Git Commit Hash: %s\n", gitHash)
+		fmt.Printf("UTC Build Time : %s\n", buildTime)
+		fmt.Printf("Golang Version : %s\n", goVersion)
+		return
+	}
 	fmt.Println("Hello, World!")
 	log.Info("Hello, World! logrus")
 	time.Sleep(9999 * time.Hour)
